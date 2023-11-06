ac3b9126efa237e68fe2159151d6187fca6bb6b0
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2023-11-06 04:25:02 -0500

    Added code for Exercise 4.3 part a

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 # In this exercise, we explore the use of local search methods to solve TSPs of the type defined in Exercise 3.30.
 
-# -----
-
 # Exercise 3.30 Description:
 
 # The traveling salesperson problem (TSP) can be solved with the minimum-spanning tree (MST) heuristic,
@@ -11,29 +9,69 @@
 # has already been constructed. The MST cost of a set of cities is the smallest sum of the link
 # costs of any tree that connects all the cities.
 
-# -----
-
 # a. Implement and test a hill-climbing method to solve TSPs.
 # b. Repeat part (a) using a genetic algorithm instead of hill climbing.
-# You may want to consult Larra˜naga et al. (1999) for some suggestions for representations.
-
-# -----
 
 # Part a - Hill-climbing algorithm implementation to solve TSPs:
 
-class HillClimbing:
+# Important Notes:
 
-    def __init__(self, tsp_problem, initial_starting_node):
+# As described in the textbook, each state in the
+# hill climbing search algorithm has all the components that make up a
+# solution even if the current state is not the solution itself
+# (the components are not in the right order in the case of tsp).
+# In this case, each state contains the current path that travels through all nodes
+# in the tsp as well as the length of that path.
+# We have found the solution when we have reached a goal state,
+# which is the path that visits all nodes/cities exactly once and returns to the starting node and has the shortest length.
+# This means that we need to begin by generating an initial path
+# that visits all nodes at random.
+
+import random
+
+
+class HillClimbingSolverForTSP:
+
+    def __init__(self, tsp_problem):
         self.tsp_problem = tsp_problem
-        self.initial_starting_node = initial_starting_node
+
+    def get_node_list(self):
+        return list(self.tsp_problem.keys())
+
+    def generate_initial_path(self):
+        node_list = self.get_node_list()
+        initial_path = []
+        starting_node = ''
+        for i in range(len(node_list)):
+            if i == 0:
+                starting_node = node_list[random.randint(0, (len(node_list) - 1))]
+                initial_path.append(starting_node)
+                node_list.remove(starting_node)
+            else:
+                placeholder_node = node_list[random.randint(0, (len(node_list) - 1))]
+                initial_path.append(placeholder_node)
+                node_list.remove(placeholder_node) #Remove the placeholder_node from the node_list to guarantee that the initial_path will contain a list of all nodes with no duplicates.
+        initial_path.append(starting_node)
+        return initial_path
+
+    def calculate_path_length(self, path):
+        path_length = 0
+        node_list = self.get_node_list()
+        for i in range(len(path)):
+            if path[i] in node_list:
+                if i < len(path) - 1:
+                    graph_edge_list = self.tsp_problem[path[i]]
+                    graph_edge =
+                    path_length += self.tsp_problem[]
+
 
     def run_hill_climbing_algorithm(self):
-        self
+        path = self.generate_initial_path()
 
 
 # A GraphSegment class that creates an object that puts together the following information:
 # a starting node, ending node, and the distance from the starting node to the ending node.
-class GraphSegment:
+class GraphEdge:
 
     def __init__(self, starting_node, ending_node, distance):
         self.starting_node = starting_node
@@ -48,13 +86,13 @@ class GraphSegment:
 
 # part a test data:
 traveling_sales_person_graph_1 = {
-    'A': [GraphSegment('A', 'B', 40), GraphSegment('A', 'C', 60), GraphSegment('A', 'D', 10)],
-    'B': [GraphSegment('B', 'A', 40), GraphSegment('B', 'C', 50), GraphSegment('B', 'D', 20)],
-    'C': [GraphSegment('C', 'A', 60), GraphSegment('C', 'B', 50), GraphSegment('C', 'D', 30)],
-    'D': [GraphSegment('D', 'A', 10), GraphSegment('D', 'B', 20), GraphSegment('D', 'C', 30)]
+    'A': [GraphEdge('A', 'B', 40), GraphEdge('A', 'C', 60), GraphEdge('A', 'D', 10)],
+    'B': [GraphEdge('B', 'A', 40), GraphEdge('B', 'C', 50), GraphEdge('B', 'D', 20)],
+    'C': [GraphEdge('C', 'A', 60), GraphEdge('C', 'B', 50), GraphEdge('C', 'D', 30)],
+    'D': [GraphEdge('D', 'A', 10), GraphEdge('D', 'B', 20), GraphEdge('D', 'C', 30)]
 }
 
-hill_climbing_test_1 = HillClimbing(traveling_sales_person_graph_1, 'A')
+hill_climbing_test_1 = HillClimbingSolverForTSP(traveling_sales_person_graph_1)
 hill_climbing_test_1.run_hill_climbing_algorithm()
 
 # part b test data:
